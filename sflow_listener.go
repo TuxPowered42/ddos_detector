06fed06a31753a4f1e54e23a61f2896aaed63acf
Kajetan Staszkiewicz <kajetan.staszkiewicz@innogames.de>
2016-06-18 16:42:57 +0200

    Break listener operation for graceful exit

--- sflow_listener.go
+++ sflow_listener.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"github.com/Cistern/sflow"
 	"net"
+	"time"
 )
 
 func sFlowParser(buffer []byte) {
@@ -19,19 +21,40 @@ func sFlowParser(buffer []byte) {
 	}
 }
 
-func sFlowListener() (err error) {
-	// Start listening UDP socket, check if it started properly
-	UDPAddr, err := net.ResolveUDPAddr("udp", ":6343")
+func sFlowListener(AppConfig app_config) (err error) {
+	defer wait.Done()
+
+	var udp_addr = fmt.Sprintf("[%s]:%d", AppConfig.SFlowConfig.Address, AppConfig.SFlowConfig.Port)
+
+	DebugLogger.Println("Binding sFlow listener to", udp_addr)
+
+	UDPAddr, err := net.ResolveUDPAddr("udp", udp_addr)
+	if err != nil {
+		ErrorLogger.Println(err)
+		return err
+	}
 	conn, err := net.ListenUDP("udp", UDPAddr)
 	if err != nil {
+		ErrorLogger.Println(err)
 		return err
 	}
 
 	var buffer []byte
-	for {
-		conn.ReadFromUDP(buffer)
-		sFlowParser(buffer)
+	for running {
+		/*
+		  Normally read would block, but we want to be able to break this
+		  loop gracefuly. So add read timeout and every 0.1s check if it is
+		  time to finish
+		*/
+		conn.SetReadDeadline(time.Now().Add(100 * time.Millisecond))
+		var read, _, err = conn.ReadFromUDP(buffer)
+		if read > 0 && err != nil {
+			sFlowParser(buffer)
+		}
+
 	}
+
+	conn.Close()
 
 	return nil
 }
